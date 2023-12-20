6d76a59305c32dcfa8302a3b8e3796941bd4f8cb
Mark Akivaev <mark220550@gmail.com>
2023-12-20 10:36:32 +0300

    Add DragButton that uses ScrollViewGestureButton from SwiftUIKit library (@danielsaidi) for ScrollView dragable item support

---
+++ Sources/DragAndDrop/DragButton.swift
@@ -0,0 +1,19 @@
+
+import SwiftUI
+
+@available(iOS 14, *)
+struct DragButton: View {
+    var body: some View {
+        ScrollViewGestureButton(
+            dragStartAction: { value in
+                print("drag start action")
+            }, dragAction: { value in
+                print("drag action \(value)")
+            }, dragEndAction: { value in
+                print("drag end action \(value)")
+            }) { isPressed in
+                Text(isPressed ? "THIS BUTTON PRESSED" : "PRESS ME TO SEE SOMETHING")
+            }
+    }
+}
+
